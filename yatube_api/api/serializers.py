44e15c6928378db576d86f24942b4aa143282be4
Mirkin K.L. <kirill555dg@gmail.com>
2026-04-25 00:01:28 +0300

    Fix error message for self-follow

--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -42,5 +42,5 @@ class FollowSerializer(serializers.ModelSerializer):
 
     def validate_following(self, value):
         if self.context["request"].user == value:
-            raise serializers.ValidationError("Нельзя подписаться на самого себя")
+            raise serializers.ValidationError("Нельзя подписаться на самого себя!")
         return value
